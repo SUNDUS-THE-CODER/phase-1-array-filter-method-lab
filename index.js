d6b74f2e87ae4aef40bdc92dded250de6ec3544f
SUNDUS-THE-CODER <119600559+SUNDUS-THE-CODER@users.noreply.github.com>
2023-01-17 23:06:52 +0000

    add all required functions

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, search) {
+   return array.filter((key) => key.toLowerCase() === search.toLowerCase());
+}
+
+function fuzzyMatch(array, search) {
+    return array.filter((key) => key.startsWith(search));
+}
+
+function matchName(array, search) {
+    return array.filter((key) => key.name === search);
+}
